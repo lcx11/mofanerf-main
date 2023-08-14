b3870d6ff4d37aa4e07dbc35e73843dc4af8ad35
吕诚鑫 <418656819@qq.com>
2023-08-15 00:46:58 +0800

    Initial commit

---
+++ download_pretrained_models.sh
@@ -0,0 +1,18 @@
+due 
+
+# check if samples.tar.gz exists
+if [ -f ./pretrained_models.tar.gz ] ; then
+echo "pretrained_models.tar.gz has already been downloaded."
+fi
+
+# download samples.tar.gz
+if [ ! -f ./pretrained_models.tar.gz ] ; then
+wget --no-check-certificate 'https://drive.google.com/u/0/uc?id=1jlhtm8BoChczSMks3WjChQ_SGLXyZ9ka&export=download' -O ./pretrained_models.tar.gz
+fi
+# wget --no-check-certificate 'https://box.nju.edu.cn/f/8e6a8048dfc14ef99c7a/?dl=1' -O ./pretrained_models.tar.gz
+
+# extract files
+mkdir -p ./logs/
+tar -zxf pretrained_models.tar.gz -C ./logs/
+echo "pretrained models have been extracted to ./logs/"
+
---
+++ test.py
@@ -0,0 +1,4 @@
+import numpy as np
+import random
+a=np.random.randn(1000,50)
+np.save("../data/factors_id.npy",a)
